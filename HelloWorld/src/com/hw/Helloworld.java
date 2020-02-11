0d2f4cfc10b4ec8ec7721aa65ed7cd38616e4451
ankita <tech.ankita@gmail.com>
2020-02-11 23:03:08 +0530

    hello world modified successfully

--- HelloWorld/src/com/hw/Helloworld.java
+++ HelloWorld/src/com/hw/Helloworld.java
@@ -2,8 +2,11 @@ package com.hw;
 
 public class Helloworld {
 
+	public static void m1() {
+		System.out.println("in m1");
+	}
 	public static void main(String[] args) {
-		
+		m1();
 		System.out.println("hello world");
 
 	}
